3736c3be7de46411fccf389155bc015431621d9e
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-04-14 13:38:13 -0700

    Update server.js

--- js/server.js
+++ js/server.js
@@ -4,23 +4,22 @@ const http = require('http'),
  fs = require('fs'),
  url = require('url');
 
-http.createServer((request, response) => {
-  var addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
+ http.createServer((request, response) => {
+   var addr = request.url,
+   q = url.parse(addr, true),
+   filePath = '';
 
-  if (q.pathname.includes('documentation')) {
-    filePath = (__dirname + '/documentation.html');
-  } else {
-    filePath = 'index.html';
-  }
-fs.appendFile('log.text', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
-  if (err) {
-    console.log(err);
-  }
-    else {
-      console.log('Added to log.');
-    }
-});
+   if (q.pathname.includes('documentation')) {
+     filePath = (__dirname + '/documentation.html');
+   } else {
+     filePath = 'index.html';
+   }
 
-}).listen(8080);
+   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
+       if (err) {
+         console.log(err);
+       } else {
+         console.log('Added to log.');
+       }
+     });
+   }).listen(8080);
