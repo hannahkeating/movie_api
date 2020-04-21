4432dccc9f0d59388a59138d75cedf34687fa778
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-04-21 11:41:24 -0700

    express();

---
+++ index.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6*/
+
+const express = require('express'),
+  morgan = require('morgan');
+const app = express();
+
+//Morgan middleware function to log all requests
+app.use(morgan('common'));
+
+//GET requests
+app.get('/', function(req, res) {
+  res.send('Welcome to Flix Fix!')
+});
+app.get('/movies', function(req, res) {
+  res.json(movies)
+});
+
+//express.static
+//"documentation.html" file from public folder
+app.use(express.static('public'));
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
