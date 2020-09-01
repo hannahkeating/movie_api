1bbe6bd6d0c6de5dd7ed934b983ea888bc814448
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-09-01 14:35:41 -0400

    main-view

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 import axios from "axios";
 
-class MainView extends React.Component {
-  constructor() {
-    //call the superclass constructor
-    //so React can initialize it
-    super();
-
-    //Initialize the state to an empty object so we can destructure it later
-    this.state = {};
-  }
-
-  //This overrides the render() method of the superclass
-  //No need to call the super() though, as it does nothing by defualt
-  render() {
-    return <div className="main-view"></div>;
-  }
-}
-
 export class MainView extends React.Component {
-  //One of the "hooks" available in a React Component
+  // One of the "hooks" available in a React Component
   componentDidMount() {
     axios
       .get("<my-api-endpoint/movies>")
       .then((response) => {
-        //Assign the result to the state
+        // Assign the result to the state
         this.setState({
           movies: response.data,
         });
@@ -35,11 +18,11 @@ export class MainView extends React.Component {
   }
 
   render() {
-    //if the state isn't initialized, this will throw on the runtime
-    //before the data is intitially loaded
+    // If the state isn't initialized, this will throw on runtime
+    // before the data is initially loaded
     const { movies } = this.state;
 
-    //Before the movies have been loaded
+    // Before the movies have been loaded
     if (!movies) return <div className="main-view" />;
 
     return (
