43bf7d4aeaf4d40e00c5b9623faf41ba43702e9b
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-09-01 14:49:11 -0400

    main-view update

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import axios from "axios";
 
+class MainView extends React.Component {
+  constructor() {
+    //Call the superclass constructor
+    // so react can initialize it
+    super();
+
+    //Initialize the state to an empty object so we can destructure it later
+    this.state = {};
+  }
+
+  //This overrides the render() method of the superclass
+  //No need to call the super() though, as it does nothing by defualt
+
+  render() {
+    return <div className="main-view"></div>;
+  }
+}
+
 export class MainView extends React.Component {
   // One of the "hooks" available in a React Component
   componentDidMount() {
