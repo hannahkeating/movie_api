7b1f28881368efe9c890067271c0bbcc7f2e7a74
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-09-02 14:46:48 -0400

    localhost update

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const { check, validationResult } = require("express-validator");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-// mongoose.connect("mongodb://localhost:27017/myFlixDB", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-mongoose.connect(process.env.CONNECTION_URI, {
+mongoose.connect("mongodb://localhost:1234/myFlixDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+// mongoose.connect(process.env.CONNECTION_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 //express.static
 //"documentation.html" file from public folder
