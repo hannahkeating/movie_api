cfd0f7438d0f7d42e3b2ccc60db9bd2228561149
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-09-02 14:59:06 -0400

    movie-view

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import axios from "axios";
 
 import { MovieCard } from "../movie-card/movie-card";
-// import { MovieView } from "../movie-view/movie-view";
-import { MovieView } from "../movie-view/movie";
+import { MovieView } from "../movie-view/movie-view";
+// import { MovieView } from "../movie-view/movie";
 
 export class MainView extends React.Component {
   constructor() {
