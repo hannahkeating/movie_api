b8c878c5690268a49c8f2dc7b7608791ae5c9361
hannahkeating <59071310+hannahkeating@users.noreply.github.com>
2020-09-02 00:07:54 -0400

    Update main-view.jsx

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 
 import { MovieCard } from "../movie-card/movie-card";
+// import { MovieView } from "../movie-view/movie-view";
 import { MovieView } from "../movie-view/movie";
 
 export class MainView extends React.Component {
